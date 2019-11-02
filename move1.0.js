3ec2b66f18d33b06ff0567cb42a30fafd36df920
lyghhh <1765967614@qq.com>
2019-11-02 17:13:33 +0800

    修复px Bug

--- move1.0.js
+++ move1.0.js
@@ -1,10 +1,17 @@
 function move(obj,name,target){
 				clearInterval(obj.timer)
 			obj.timer=	setInterval(function(){
+				let speed=(target-parseInt(getComputedStyle(obj)[name]))/10
+				// 当不能整除时,运动到最后10个像素时,1-9像素除以10等于小于1的小数,向下取整speed=0即停止运动,反之同理
+				if (speed>0) {
+					speed=Math.ceil(speed)
+				} else{
+					speed=Math.floor(speed)
+				}
 					if (parseInt(getComputedStyle(obj)[name])==target) {
 						clearInterval(obj.timer)
 					} else{
-						let speed=(target-parseInt(getComputedStyle(obj)[name]))/10
+						
 						obj.style[name]=parseInt(getComputedStyle(obj)[name])+speed+'px'
 					}
 				},30)
