1ddc33b6d6d2664158a7d6477cc313e9373c433e
lyghhh <1765967614@qq.com>
2019-11-02 17:16:47 +0800

    三元运算符

--- move1.0.js
+++ move1.0.js
@@ -2,14 +2,15 @@ function move(obj,name,target,fn){
 				clearInterval(obj.timer)
 			obj.timer=	setInterval(function(){
 				let speed=(target-parseInt(getComputedStyle(obj)[name]))/10
-				if (speed>0) {
-					speed=Math.ceil(speed)
-				} else{
-					speed=Math.floor(speed)
-				}
+				// if (speed>0) {
+				// 	speed=Math.ceil(speed)
+				// } else{
+				// 	speed=Math.floor(speed)
+				// }
+				// 三元运算符
+				speed= (speed>0) ? Math.ceil(speed) :Math.floor(speed)
 					if (parseInt(getComputedStyle(obj)[name])==target) {
 						clearInterval(obj.timer)
-						// 链式运动 判断是否存在fn,存在时即执行
 						if(fn){fn()}
 					} else{
 						
