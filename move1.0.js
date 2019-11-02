f777a1b246e806c401176160a33dc11c6147377d
lyghhh <1765967614@qq.com>
2019-11-02 17:25:12 +0800

    透明度 宽高 背景 together

--- move1.0.js
+++ move1.0.js
@@ -3,8 +3,11 @@ function move(obj,name,target,fn){
 			obj.timer=	setInterval(function(){
 				if (name=='opacity') {
 					cur=getComputedStyle(obj)[name]*100
-				} else{
-					cur=parseInt(getComputedStyle(obj)[name])
+				} else if(name=='width'||name=='height'||name=='border-radius'||name=='border-width'||name=='font-size'){
+					 cur=parseInt(getComputedStyle(obj)[name])
+				}
+				else{
+					cur=getComputedStyle(obj)[name]
 				}
 				let speed=(target-cur)/10
 				speed= (speed>0) ? Math.ceil(speed) :Math.floor(speed)
@@ -14,8 +17,11 @@ function move(obj,name,target,fn){
 					} else{
 						if (name=='opacity') {
 							obj.style[name]=(cur+speed)/100
-						} else{
-							obj.style[name]=cur+speed+'px'
+						} else if(name=='width'||name=='height'||name=='border-radius'||name=='border-width'||name=='font-size'){
+							 obj.style[name]=cur+speed+'px'
+						}
+						else{
+						obj.style[name]=target
 						}
 						
 					}
